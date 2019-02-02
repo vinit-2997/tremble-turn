774acf4492b524140ab4f2c66bfcd24c527ce50f
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2019-02-02 21:28:52 +0530

    volley requets working

--- app/src/main/java/com/trembleturn/trembleturn/webservice/Routes.java
+++ app/src/main/java/com/trembleturn/trembleturn/webservice/Routes.java
@@ -5,10 +5,11 @@ import org.json.JSONObject;
 public interface Routes {
 
     String API_URL = "https://maps.googleapis.com/maps/api/directions/json?";
+    String API_KEY = "AIzaSyD61OtmkLZi8l_F3sVVe0R3DH5QfaZ3kbc";
 
-    void makeStringGetRequest(String url, boolean showDialog);
+    void makeStringGetRequest(String url);
 
-    void makeJsonPostRequest(String url, JSONObject jsonRequest, boolean showDialog);
+    void makeJsonPostRequest(String url, JSONObject jsonRequest);
 
-    void makeJsonPutRequest(String url, JSONObject jsonRequest, boolean showDialog);
+    void makeJsonPutRequest(String url, JSONObject jsonRequest);
 }
